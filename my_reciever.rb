ea82b697e04a10810d7b8d02e4d66f1c07fb3113
birger lie <birger.lie@gmail.com>
2011-10-13 13:40:03 +0200

    kafka reciever

--- my_reciever.rb
+++ my_reciever.rb
@@ -1,3 +1,18 @@
+require "kafka"
 class MyReciever
-  # To change this template use File | Settings | File Templates.
+  def initialize()
+
+  end
+
+  def listen()
+    consumer = Kafka::Consumer.new({:host=>'datanode25.companybook.no',:topic=>'test', :partition=>0})
+    consumer.loop do |messages|
+      puts "Received"
+      puts messages.inspect
+
+    end
+  end
 end
+
+
+MyReciever.new.listen
